30d4a375817ca37f8eaf87acbd2c2e5361d42a63
mulveyj <github@joemulvey.com>
2017-06-29 12:07:47 +0100

    First commit - layout

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,31 @@ class App extends React.Component {
       <div className="component-App">
         <NavBar />
         <div className='main container-fluid'>
-          {/* Your components go here */}
+          <div className='area-left col-md-3'>
+            <div className='component-profile'>
+              Profile
+            </div>
+            <div className='component-trends'>
+              Trends
+            </div>
+          </div>
+          <div className='area-centre col-md-6'>
+            <div className='component-makeTweet'>
+              Make Tweet
+            </div>
+            <div className='component-TweetList'>
+              TweetList
+            </div>
+          </div>
+          <div className='area-right col-md-3'>
+            <div className='component-follow'>
+              Follow
+            </div>
+            <div className='component-policy'>
+              Policy
+            </div>
+
+          </div>
         </div>
       </div>
     );
