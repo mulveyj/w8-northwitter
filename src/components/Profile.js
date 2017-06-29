44f711e9a9936bd75d9c4b6a259ffddc9365f723
mulveyj <github@joemulvey.com>
2017-06-29 13:00:54 +0100

    profile updated

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,24 +6,28 @@ class Profile extends React.Component {
         return (
             <div className='component-profile'>
                 <div className='topRow'>
-                    <div className='profileImage col-md-1'>
-                        I
+                    <div className='profileImage col-xs-4'>
+                        <img id='donald' src="https://az616578.vo.msecnd.net/files/responsive/cover/main/desktop/2016/06/13/6360139435793044861461393096_Donald-Trump-prune-face.jpg" alt="Donald Trump"/>
                     </div>
-                    <div className='profileName col-md-2'>
-                        P
+                    <div className='profileName col-xs-8'>
+                        <p className='user'>Donald Trump</p>
+                        <p className='user-handle'>@US-GOV</p>
                     </div>
                 </div>
-                <ul className='bottomRow'>
-                    <li className='tweets'>
-                        <p>T 6645</p>
-                    </li>
-                    <li className='following'>
-                        <p>F 6645</p>
-                    </li>
-                    <li className='followers'>
-                        <p> 6645</p>
-                    </li>
-                </ul>
+                <div className='bottomRow'>
+                    <div className='stat col-xs-4'>
+                        <p>Tweets</p>
+                        <p>664</p>
+                    </div>
+                    <div className='stat col-xs-4'>
+                        <p>Following</p>
+                        <p> 6773</p>
+                    </div>
+                    <div className='stat col-xs-4'>
+                        <p>Followers</p>
+                        <p>66464</p>
+                    </div>
+                </div>
             </div>
         );
     }
